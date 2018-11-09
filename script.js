c77b3a8cb2e8f3242b7ac0f41a9a1dc8dd58e7a3
traviegotswag <travisng@hotmail.sg>
2018-11-09 18:43:33 +0800

    Basic deliverables completed

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
-class List extends React.Component {
+class App extends React.Component {
   constructor(){
     super()
     this.changeHandler = this.changeHandler.bind( this );
+    this.submitHandler = this.submitHandler.bind( this );
   }
 
   state = {
-    list : [],
-    word : ""
+    task : "",
+    list : []
   }
 
   changeHandler(event){
-    this.setState({word:event.target.value});
-    console.log("change", event.target.value);
+    this.setState({task:event.target.value});
+    console.log("logging", event.target.value);
+  }
+
+  submitHandler(event){
+    event.preventDefault();
+    this.setState({list: [...this.state.list, this.state.task]});
   }
 
   render() {
       // render the list with a map() here
 
-      console.log("rendering");
+      // console.log("rendering");
       return (
         <div className="list">
-          <input onChange={this.changeHandler} value={this.state.word}/>
-          <button>add item</button>
+          <form className="App" onSubmit={this.submitHandler}>
+            <input onChange={this.changeHandler} value={this.state.task}/>
+            <button>Add Item</button>
+          </form>
+          <List list={this.state.list}/>
         </div>
       );
   }
 }
 
+class List extends React.Component {
+
+  render() {
+    
+    const ToDoList = this.props.list.map(task => (
+        <li key={task.id}>{task}</li>
+    ));
+
+    return (
+        <ul>{ToDoList}</ul>
+    )
+  }
+  // state = {
+  //   task : "",
+  //   list : []
+  // }
+
+}
+
 ReactDOM.render(
-    <List/>,
+    <App/>,
     document.getElementById('root')
 );
 
